8cf6cc45a974c5eabfe7707043c23c0a34272dd0
Pavan Kumar <Pavan@Pavans-MacBook-Pro.local>
2015-02-25 00:17:35 -0800

    trying to serve dynamic content but not yet done

--- web.js
+++ web.js
@@ -3,9 +3,12 @@ var express = require('express');
 var app = express();
 
 app.set('port', (process.env.PORT || 5000));
+app.use(express.static(__dirname + '/public'));
 
 app.get('/', function(request, response) {
   response.sendFile('index.html', { root: __dirname });
+  // response.render('index');
+  
 });
 
 app.listen(app.get('port'), function() {
